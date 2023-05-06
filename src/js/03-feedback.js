69a906f06178193775a37b2596e63f15ba6181d7
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-05-06 22:27:45 +0200

    +

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -13,7 +13,8 @@ const KEY_SRORAGE_INPUT = "feedback-form-state";
 // з полями email і message, у яких зберігай поточні значення полів форми.
 // Нехай ключем для сховища буде рядок "feedback-form-state".
 form.addEventListener('input', saveCurrentValue);
-
+let feedbackUser;
+let feedbackUserJSON;
 function saveCurrentValue(evt) {
     if (inputEmail.value !== "" && textareaMessage.value !== "") {
         feedbackUser = {
@@ -25,40 +26,37 @@ function saveCurrentValue(evt) {
     }
 };
 
+
 // 2.   Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані,
 // заповнюй ними поля форми.В іншому випадку поля повинні бути порожніми.
 
 const getFeedbackUser = localStorage.getItem(KEY_SRORAGE_INPUT);
 // console.log(getFeedbackUser);
-// console.log(feedbackUserParseJSON);
 const feedbackUserParseJSON = JSON.parse(getFeedbackUser);
 if (getFeedbackUser !== '') {
     inputEmail.value = feedbackUserParseJSON.email;
     textareaMessage.value = feedbackUserParseJSON.message;
 }
-// try {
-//     feedbackUserParseJSON = JSON.parse(getFeedbackUser);
-//     if (getFeedbackUser !== '') {
-//         inputEmail.value = feedbackUserParseJSON.email;
-//         textareaMessage.value = feedbackUserParseJSON.message;
-//     }
-// } catch(error) {
-//     console.error(error.name);
-// }
+// console.log(feedbackUserParseJSON);
+
 
 // 3.Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт 
 // з полями email, message та їхніми поточними значеннями.
 
 form.addEventListener('submit', handleSubmit);
 
-function handleSubmit (event) {
+function handleSubmit(event) {
     event.preventDefault();
     if (inputEmail.value === '' || textareaMessage.value === '') {
         return alert('Please fill in all fields!');
-    } else {
-        console.log(feedbackUserParseJSON);
-        localStorage.removeItem(KEY_SRORAGE_INPUT);
-        inputEmail.value = '';
-        textareaMessage.value = '';
+
     }
+    console.log(feedbackUserParseJSON);
+    localStorage.removeItem(KEY_SRORAGE_INPUT);
+    inputEmail.value = '';
+    textareaMessage.value = '';
 };
+
+
+// 4.Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд.
+// Для цього додай до проекту і використовуй бібліотеку lodash.throttle.

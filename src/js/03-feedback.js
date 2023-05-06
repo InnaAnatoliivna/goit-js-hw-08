6fa445d935813828f257d456880e8a4327e3bda5
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-05-06 21:46:41 +0200

    +

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,64 @@
+import { throttle } from "lodash";
+//Напиши скрипт, який буде зберігати значення полів у локальне сховище, 
+//kоли користувач щось друкує.
 
+const form = document.querySelector('.feedback-form')
+const inputEmail = document.querySelector('input');
+const textareaMessage = document.querySelector('textarea');
+// const button = document.querySelector('button');
+const KEY_SRORAGE_INPUT = "feedback-form-state";
+
+
+// 1.Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт 
+// з полями email і message, у яких зберігай поточні значення полів форми.
+// Нехай ключем для сховища буде рядок "feedback-form-state".
+form.addEventListener('input', saveCurrentValue);
+
+function saveCurrentValue(evt) {
+    if (inputEmail.value !== "" && textareaMessage.value !== "") {
+        feedbackUser = {
+            email: inputEmail.value,
+            message: textareaMessage.value
+        };
+        feedbackUserJSON = JSON.stringify(feedbackUser);
+        localStorage.setItem(KEY_SRORAGE_INPUT, feedbackUserJSON);
+    }
+};
+
+// 2.   Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані,
+// заповнюй ними поля форми.В іншому випадку поля повинні бути порожніми.
+
+const getFeedbackUser = localStorage.getItem(KEY_SRORAGE_INPUT);
+// console.log(getFeedbackUser);
+// console.log(feedbackUserParseJSON);
+const feedbackUserParseJSON = JSON.parse(getFeedbackUser);
+if (getFeedbackUser !== '') {
+    inputEmail.value = feedbackUserParseJSON.email;
+    textareaMessage.value = feedbackUserParseJSON.message;
+}
+// try {
+//     feedbackUserParseJSON = JSON.parse(getFeedbackUser);
+//     if (getFeedbackUser !== '') {
+//         inputEmail.value = feedbackUserParseJSON.email;
+//         textareaMessage.value = feedbackUserParseJSON.message;
+//     }
+// } catch(error) {
+//     console.error(error.name);
+// }
+
+// 3.Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт 
+// з полями email, message та їхніми поточними значеннями.
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit (event) {
+    event.preventDefault();
+    if (inputEmail.value === '' || textareaMessage.value === '') {
+        return alert('Please fill in all fields!');
+    } else {
+        console.log(feedbackUserParseJSON);
+        localStorage.removeItem(KEY_SRORAGE_INPUT);
+        inputEmail.value = '';
+        textareaMessage.value = '';
+    }
+};

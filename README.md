aee0d1c7f5c48fb438a9fa96be63f48847a534ab
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-05-08 13:25:44 +0200

    3 task on review

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+1. create object without construction if {}
+    // feedbackUser = {};
+    // feedbackUser.email = inputEmail.value;
+    // feedbackUser.message = textareaMessage.value;
+
+    // feedbackUserJSON = JSON.stringify(feedbackUser);
+    // localStorage.setItem(KEY_SRORAGE_INPUT, feedbackUserJSON);
+
+
+3. feedback
 // try {
 //     feedbackUserParseJSON = JSON.parse(getFeedbackUser);
 //     if (getFeedbackUser !== '') {
@@ -7,3 +17,9 @@
 // } catch(error) {
 //     console.error(error.name);
 // }
+or
+// if (getFeedbackUser !== '') {
+//     const feedbackUserParseJSON = JSON.parse(getFeedbackUser);
+//     inputEmail.value = feedbackUserParseJSON.email;
+//     textareaMessage.value = feedbackUserParseJSON.message;
+// console.log(feedbackUserParseJSON);}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,18 +5,21 @@ import { throttle } from "lodash";
 const form = document.querySelector('.feedback-form')
 const inputEmail = document.querySelector('input');
 const textareaMessage = document.querySelector('textarea');
-// const button = document.querySelector('button');
 const KEY_SRORAGE_INPUT = "feedback-form-state";
 
 
 // 1.Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт 
 // з полями email і message, у яких зберігай поточні значення полів форми.
 // Нехай ключем для сховища буде рядок "feedback-form-state".
-form.addEventListener('input', saveCurrentValue);
+// 4.Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд.
+// Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
+
+form.addEventListener('input', throttle(saveCurrentValue, 500));
 let feedbackUser;
 let feedbackUserJSON;
+let feedbackUserParseJSON;
 function saveCurrentValue(evt) {
-    if (inputEmail.value !== "" && textareaMessage.value !== "") {
+    if (inputEmail.value !== "" || textareaMessage.value !== "") {
         feedbackUser = {
             email: inputEmail.value,
             message: textareaMessage.value
@@ -31,13 +34,18 @@ function saveCurrentValue(evt) {
 // заповнюй ними поля форми.В іншому випадку поля повинні бути порожніми.
 
 const getFeedbackUser = localStorage.getItem(KEY_SRORAGE_INPUT);
+
 // console.log(getFeedbackUser);
-const feedbackUserParseJSON = JSON.parse(getFeedbackUser);
-if (getFeedbackUser !== '') {
-    inputEmail.value = feedbackUserParseJSON.email;
-    textareaMessage.value = feedbackUserParseJSON.message;
+
+try {
+    feedbackUserParseJSON = JSON.parse(getFeedbackUser);
+    if (getFeedbackUser !== '') {
+        inputEmail.value = feedbackUserParseJSON.email;
+        textareaMessage.value = feedbackUserParseJSON.message;
+    }
+} catch (error) {
+    console.error(error.message);
 }
-// console.log(feedbackUserParseJSON);
 
 
 // 3.Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт 
@@ -49,14 +57,9 @@ function handleSubmit(event) {
     event.preventDefault();
     if (inputEmail.value === '' || textareaMessage.value === '') {
         return alert('Please fill in all fields!');
-
     }
     console.log(feedbackUserParseJSON);
     localStorage.removeItem(KEY_SRORAGE_INPUT);
     inputEmail.value = '';
     textareaMessage.value = '';
 };
-
-
-// 4.Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд.
-// Для цього додай до проекту і використовуй бібліотеку lodash.throttle.
